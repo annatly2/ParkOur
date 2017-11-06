54fa3cb16914cecb6f7ad390f53287456a7f876f
Anna Ly <annatly2@gmail.com>
2017-11-05 18:15:42 -0800

    nov 5

--- app.js
+++ app.js
@@ -1,52 +1,55 @@
 $(document).ready(function(){
 
+  var UserLatitude;
+  var UserLongitude;
+
+  //asks users for their location
   function getLocation() {
-    //Make sure the broswer supports the HTML 5 geolocation method.
     if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(setPosition);
     } else { 
-        //We may also have to set a default UserLatitute and UserLongtitude here to 
-        //make sure we don't error out if the the browser does not support geolocation.
+        //if the the browser does not support geolocation
         console.log("Geolocation is not supported by this browser.");
     }
 }
-
 
 function setPosition(position) {
   UserLatitude = position.coords.latitude
   UserLongitude = position.coords.longitude;
   
 }
-    //Run the procedure to get the user's location.  
-    //We need to to figure out what we are going to do if the user does not provide cordinates.
-    //WE should probably set a default longitude and latitude.
+    //default location is UCSD
     UserLatitude = 32.711652;
     UserLongitude = -117.161887;
     getLocation();
 
-})
-
+var newAPIkey = "AIzaSyCnvwAzxz0QHE-3VbCv_odGXd8ThkidDvc";
 
 var newQueryURL = "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + UserLatitude + "," + UserLongitude + "&radius=8000&type=parking&key="+ newAPIkey;
 
-
-//request you can put into postman
+//request you can put into postman to view the JSON object
 var postmanRequest = "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=32.711652,-117.161887&radius=8000&type=parking&key=AIzaSyCnvwAzxz0QHE-3VbCv_odGXd8ThkidDvc";
-
 
 $.ajax({
   url: newQueryURL, 
   method: "GET",
   dataType: "json"                 
 }).done(function(response) {
+  var i = 0;
+  var answerSelected = false;
 
-  var name;
+  //loops through the entire request and prints the name and address in the console
+  for(var i = 0; i < response.results.length; i ++){
+    var name = response.results[i].name;
+    var vicinity = response.results[i].vicinity;
 
-)}
+    console.log(name);
+    console.log(vicinity);
 
-//API KEY FOR Google places:
+      //displays the last result on the html file.
+    $("#name").html(name);
+    $("#vicinity").html(vicinity);
+  }
+});
 
-//var apiKey = "AIzaSyDGije4oEmFObRp1TQgyquNCJjeos9sq78";
-
-
-var newAPIkey = "AIzaSyCnvwAzxz0QHE-3VbCv_odGXd8ThkidDvc";
+});
